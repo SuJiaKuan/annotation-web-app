d3a2ced86a1bf27461c2dae634ce1a1821c4105f
SuJiaKuan <jiakuan.su@toppano.in>
2018-03-02 12:44:20 +0800

    Update data reducer

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -4,35 +4,7 @@ import { ADD_DATASET_REQUEST, ADD_DATASET_SUCCESS, ADD_DATASET_FAIL } from 'cons
 
 const initialState = {
   isFetch: false,
-  /*
   datasetList: [],
-  */
-  datasetList: [
-    {
-      id: 'jill',
-      name: 'Jill Dataset',
-      description: 'The dataset that contains many Jills',
-      images: [
-        'http://jillsantopolo.com/wordpress/wp-content/uploads/2016/07/Jill-8730-wall.crop_-300x295.jpg',
-        'https://www.law.hawaii.edu/sites/www.law.hawaii.edu/files/styles/headshot_large/public/people/17248/Ramsfield_Jill.jpg?itok=4Ba6_nY1',
-        'https://hope.edu/graphics/prportraits/JillNutt.jpg',
-        'https://vg-images.condecdn.net/image/qdZr33EDBvm/crop/810/f/jill-mcdonald-vogue-5may17-pa.jpg',
-        'https://cdnimages.barry.edu/includes/img/social-work/bio/jilllevenson.jpg',
-      ],
-    },
-    {
-      id: 'jill2',
-      name: 'Jill Dataset 2',
-      description: 'The dataset that contains many Jills',
-      images: [
-        'http://jillsantopolo.com/wordpress/wp-content/uploads/2016/07/Jill-8730-wall.crop_-300x295.jpg',
-        'https://www.law.hawaii.edu/sites/www.law.hawaii.edu/files/styles/headshot_large/public/people/17248/Ramsfield_Jill.jpg?itok=4Ba6_nY1',
-        'https://hope.edu/graphics/prportraits/JillNutt.jpg',
-        'https://vg-images.condecdn.net/image/qdZr33EDBvm/crop/810/f/jill-mcdonald-vogue-5may17-pa.jpg',
-        'https://cdnimages.barry.edu/includes/img/social-work/bio/jilllevenson.jpg',
-      ],
-    },
-  ],
 }
 
 export default function data(state = initialState, action) {
